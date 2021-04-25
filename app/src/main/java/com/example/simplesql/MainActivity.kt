d00e2c1b08ecf2d6c09e982194a020d03de2b536
ba1999 <andrej.boldt@outllok>
2021-04-25 17:35:42 +0200

    Vorbereiten der MainActivity

--- app/src/main/java/com/example/simplesql/MainActivity.kt
+++ app/src/main/java/com/example/simplesql/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.simplesql
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import splitties.toast.toast
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     private val etRoom : TextView by lazy{ findViewById(R.id.etRoom) }
     private val etTemp : TextView by lazy{ findViewById(R.id.etTemp) }
@@ -28,8 +30,10 @@ class MainActivity : AppCompatActivity() {
 
         if (sRaum.isEmpty() || sTemp.isEmpty() || sHum.isEmpty() || sDate.isEmpty()) {
             toast(getString(R.string.eingabefehler))
+            Log.d(TAG, getString(R.string.eingabefehler))
         } else {
             toast(getString(R.string.gespeichert))
+            Log.i(TAG, "$sRaum $sTemp $sHum $sDate")
         }
     }
 }
