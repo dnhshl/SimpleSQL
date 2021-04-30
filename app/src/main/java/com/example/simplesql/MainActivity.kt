df78725bd1d9e5299ff9994c8711c170c580dfa2
ba1999 <andrej.boldt@outllok>
2021-04-30 15:04:05 +0200

    Hinzufügen von Daten

--- app/src/main/java/com/example/simplesql/MainActivity.kt
+++ app/src/main/java/com/example/simplesql/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simplesql
 
+import android.database.sqlite.SQLiteConstraintException
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -15,10 +16,17 @@ class MainActivity : AppCompatActivity() {
     private val etAir : TextView by lazy{ findViewById(R.id.etAir) }
     private val etDate : TextView by lazy{ findViewById(R.id.etDate) }
 
+    private lateinit var mDb: AppDatabase
+    private lateinit var mWeatherDao: WeatherDAO
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mDb = AppDatabase.getAppDatabase(context = this)!!
+        mWeatherDao = mDb.getWeatherDao()
+
     }
 
     fun onClickButton(v: View?) {
@@ -31,9 +39,22 @@ class MainActivity : AppCompatActivity() {
         if (sRaum.isEmpty() || sTemp.isEmpty() || sHum.isEmpty() || sDate.isEmpty()) {
             toast(getString(R.string.eingabefehler))
             Log.d(TAG, getString(R.string.eingabefehler))
+
         } else {
             toast(getString(R.string.gespeichert))
+            val temp = sTemp.toInt()
+            val hum = sHum.toInt()
+            insert(Weather(room = sRaum, temp = temp, hum = hum, date = sDate))
             Log.i(TAG, "$sRaum $sTemp $sHum $sDate")
+        }
+    }
+
+    private fun insert(weather: Weather) {
+        try {
+            mWeatherDao.insert(weather)
+            Log.i(TAG, "${weather} successful")
+        } catch (e: SQLiteConstraintException) {
+            Log.e(TAG, "$e no success!")
         }
     }
 }
